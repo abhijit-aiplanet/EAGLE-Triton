2c19860a7b9d838700606c25f11ed8afbc7e6116
abhijit-aiplanet <abhijit@aiplanet.com>
2024-10-01 16:25:24 +0530

    Update ea_model.py

--- eagle/model/ea_model.py
+++ eagle/model/ea_model.py
@@ -65,7 +65,7 @@ class EaModel(nn.Module):
 
         else:
             self.ea_layer.diff_device = False
-        self.ea_layer.load_state_dict(ea_layer_state_dict, strict=True)
+        self.ea_layer.load_state_dict(ea_layer_state_dict, strict=False)
         self.ea_layer.to(self.base_model.dtype).to(device)
         self.ea_layer.init_tree()
 
